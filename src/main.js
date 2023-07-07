061b0978887db87d2396a7ea754b5ee87426fea7
yangjj <yang_jia_jian@163.com>
2023-07-07 14:52:40 +0800

    build successfully

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 // -------------router--------------
 import router from './router'
-
+// -------------icons--------------
+import '@/static/icons/style.css'
+// -------------axios--------------
 
 const app = createApp(App)
 app.use(ElementPlus).use(router)
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,18 +1,42 @@
 <template>
   <h1>Hello, YJJ!</h1>
-  <el-button @click="submit">Default</el-button>
+  <el-input v-model="userInfo.user_name" placeholder="请输入用户名"></el-input>
+  <el-input v-model="userInfo.user_passwd" placeholder="请输入密码" type="password" show-password></el-input>
+  <el-button @click="login">登录</el-button>
 </template>
 
 <script>
+import {reactive} from "vue";
+import axios from "axios";
+
 export default {
   setup(){
-    function submit(){
+    let userInfo = reactive({
+      user_name:'',
+      user_passwd:'',
+    })
+    function login(){
       //TODO:和后端交互
-      alert("上传成功！")
+      axios({
+        method: 'POST',
+        url: 'http://localhost:9090/user/login',
+        headers:{
+          "Content-Type":"application/json",
+        },
+        data:JSON.stringify(userInfo)
+      }).then(resp => {
+        if(resp.data.code === 200){
+          console.log(resp.data)
+          localStorage.setItem("user",JSON.stringify(resp.data.data))
+          alert("登录成功!")
+        } else
+          alert("登录失败!")
+      })
+
     }
     return{
-      submit,
-
+      userInfo,
+      login,
     }
   }
 }
